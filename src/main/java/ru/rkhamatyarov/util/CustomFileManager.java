de5488b487513711789886def6f58d6af9a6cfa0
riposite <rraamil@gmail.com>
2025-06-24 21:17:59 +0300

    Add github ci workflow with building and testing application and add dependabot, fix formatting linter issues

--- src/main/java/ru/rkhamatyarov/util/CustomFileManager.java
+++ src/main/java/ru/rkhamatyarov/util/CustomFileManager.java
@@ -11,26 +11,64 @@ import java.nio.file.StandardOpenOption;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.nio.file.Paths.get;
 
-;
-
+/**
+ * The CustomFileManager class provides methods for managing file operations,
+ * such as writing content to files.
+ */
 public class CustomFileManager {
-    private static final Logger log = LoggerFactory.getLogger(CustomFileManager.class);
-    private static final CustomFileManager customFileManager = new CustomFileManager();
 
-    public CustomFileManager() {}
+    /**
+     * Logger for the CustomFileManager class.
+     */
+    private static final Logger LOG = LoggerFactory
+            .getLogger(CustomFileManager.class);
 
-    public static CustomFileManager getInstance() {
-        return customFileManager;
+    /**
+     * Singleton instance of CustomFileManager.
+     */
+    private static final CustomFileManager CUSTOM_FILE_MANAGER
+            = new CustomFileManager();
+
+    /**
+     * Private constructor to prevent instantiation.
+     */
+    public CustomFileManager() {
     }
 
-    public Boolean write(String key, String content) throws IOException {
+    /**
+     * Returns the singleton instance of CustomFileManager.
+     *
+     * @return the singleton instance of CustomFileManager
+     */
+    public static CustomFileManager getInstance() {
+        return CUSTOM_FILE_MANAGER;
+    }
+
+    /**
+     * Writes the specified content to a file identified by the given key.
+     * If the file already exists, it will be deleted before writing.
+     *
+     * @param key     the path to the file where content will be written
+     * @param content the content to write to the file
+     * @return true if the content was successfully written, false otherwise
+     * @throws IOException if an I/O error occurs
+     */
+    public Boolean write(
+            final String key,
+            final String content
+    ) throws IOException {
         Path p = get(key);
         if (Files.exists(p)) {
             Files.delete(p);
         }
 
-        Path path = Files.write(p, (content + "\n").getBytes(UTF_8), StandardOpenOption.CREATE,StandardOpenOption.APPEND);
-        log.info("Content wrote on file {}", path.toAbsolutePath().toString());
+        Path path = Files.write(
+                p,
+                (content + "\n").getBytes(UTF_8),
+                StandardOpenOption.CREATE,
+                StandardOpenOption.APPEND
+        );
+        LOG.info("Content wrote on file {}", path.toAbsolutePath());
         return Files.exists(path);
     }
 }
---
+++ src/main/java/ru/rkhamatyarov/util/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains utility classes
+ * for file management in the Markov chain text generation application.
+ */
+package ru.rkhamatyarov.util;
